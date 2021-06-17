5a508d59ae54bacbedd6bc80103677fe640a4f25
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-17 22:16:47 +0300

    Update database.py
    Rewrite in OOP style

--- database.py
+++ database.py
@@ -1,15 +1,36 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, results
 
 
 class Client:
-    def __init__(self):
-        cluster = MongoClient('mongodb+srv://mezgoodle:<password>@githubhelper.2tnee.mongodb.net/githubhelper'
+    def __init__(self, password):
+        cluster = MongoClient(f'mongodb+srv://mezgoodle:{password}@githubhelper.2tnee.mongodb.net/githubhelper'
                               '?retryWrites=true&w=majority')
         db = cluster['githubhelper']
         self.collection = db['tokens']
 
-    def insert(self, data: dict):
-        self.collection.insert_one(data)
+    def insert(self, data: dict) -> results.InsertOneResult:
+        try:
+            return self.collection.insert_one(data)
+        except Exception as e:
+            print('Error:', e)
 
-# token = {'token': 'ghp_31eqweqw', 'telegram_id': 'test_id'}
-# collection.insert_one(token)
+    def get(self, query: dict) -> dict:
+        try:
+            return self.collection.find_one(query)
+        except Exception as e:
+            print('Error:', e)
+
+    def update(self, query: dict, data: dict) -> results.UpdateResult:
+        try:
+            return self.collection.update_one(query, data)
+        except Exception as e:
+            print('Error:', e)
+
+    def delete(self, query: dict) -> results.DeleteResult:
+        try:
+            return self.collection.delete_one(query)
+        except Exception as e:
+            print('Error:', e)
+
+# client = Client('password')
+# print(client.update({'token': "ghp_31eqweqw"}, {'$set': {'telegram_id': 'tg_test_id'}}))
