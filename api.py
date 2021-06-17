885c4db6fd95d21ad1469d85396512ec44aacd2b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-17 17:25:09 +0300

    Update api.py
    Rewrite in class

--- api.py
+++ api.py
@@ -1,8 +1,18 @@
 from github import Github
 
-g = Github('TOKEN')
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    # to see all the available attributes and methods
-    # print(dir(repo))
+class API:
+    def __init__(self, token):
+        self.g = Github(token)
+
+    def get_user_info(self) -> str:
+        """
+        Method that returns string with information about user
+        :return: string with information
+        """
+        user = self.g.get_user()
+        return f'You have been authenticated with login _{user.login}_ as _{user.name}_'
+
+    def get_repos(self):
+        pass
+
