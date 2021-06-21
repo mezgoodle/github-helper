f4bdab3df44d46a4e973183f65e8d49f82ea7c0e
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-21 14:00:24 +0300

    Update api.py
    Set user as field

--- api.py
+++ api.py
@@ -5,28 +5,29 @@ from github.GithubException import UnknownObjectException
 class Api:
     def __init__(self, token):
         self.g = Github(token)
+        self.user = self.g.get_user()
 
     def get_user_info(self) -> str:
         """
         Method that returns string with information about user
         :return: string with information
         """
-        user = self.g.get_user()
-        return f'You have been authenticated with login *{user.login}* as *{user.name}*'
+        return f'You have been authenticated with login *{self.user.login}* as *{self.user.name}*'
 
     def get_repos(self) -> str:
-        user = self.g.get_user()
-        repos = user.get_repos()
+        repos = self.user.get_repos()
         result = ''
+        index = 1
         for repo in repos:
             if not repo.archived:
-                result += f'- {repo.name}. [Link]({repo.html_url}). Total issues and prs: {repo.get_issues().totalCount}\n '
+                result += f'{index}. {repo.name}. [Link]({repo.html_url}). ' \
+                          f'Total issues and prs: {repo.get_issues().totalCount}\n'
+                index += 1
         return result
 
     def get_repo(self, repo_name: str) -> dict:
-        user = self.g.get_user()
         try:
-            repo = user.get_repo(repo_name)
+            repo = self.user.get_repo(repo_name)
             return {
                 'name': repo.name,
                 'url': repo.html_url,
@@ -36,3 +37,13 @@ class Api:
             }
         except UnknownObjectException:
             return {}
+
+    def get_issues(self) -> str:
+        issues = self.user.get_issues()
+        result = ''
+        for issue in issues:
+            if not issue.pull_request:
+                result += f'- _{issue.title}_ [#{issue.number}]({issue.html_url}). ' \
+                          f'[Link to repository]({issue.repository.html_url}). Created: _{issue.created_at}_. ' \
+                          f'Author: _{issue.user.name}_\n'
+        return result
