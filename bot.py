e657161e56ffb62f9b8602cfc5d2827dc7a0c182
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-21 16:03:40 +0300

    Update bot.py
    Update callback handler

--- bot.py
+++ bot.py
@@ -16,6 +16,43 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
+async def decrypt_token(user_id: int) -> str:
+    db = Client('password')
+    data = db.get({'telegram_id': user_id})
+    hasher = Hasher()
+    try:
+        encrypted_token = data.get('token')
+        decrypted_token = hasher.decrypt_message(encrypted_token)
+        return decrypted_token
+    except TypeError:
+        return ''
+
+
+@dp.callback_query_handler(lambda c: c.data)
+async def process_callback(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    user_id = callback_query.from_user.id
+    decrypted_token = await decrypt_token(user_id)
+    if decrypted_token:
+        info = Api(decrypted_token)
+        data = info.get_repo(callback_query.data)
+        inline_keyboard = types.InlineKeyboardMarkup()
+        for issue in data['issues']:
+            if not issue.pull_request:
+                button = types.InlineKeyboardButton(f'Issue #{issue.number} - {issue.title}', url=issue.html_url)
+            else:
+                button = types.InlineKeyboardButton(f'Pull request #{issue.number} - {issue.title}',
+                                                    url=issue.html_url)
+            inline_keyboard.add(button)
+        final_text = f"Name: *{data['name']}*\nLink: [click here]({data['url']})\n" \
+                     f"Stars: *{data['stars']}*\nTotal issues and prs: *{data['count_of_issues']}*"
+        await bot.send_message(callback_query.from_user.id, final_text, reply_markup=inline_keyboard,
+                               parse_mode='Markdown')
+    else:
+        return await bot.send_message(callback_query.from_user.id,
+                                      'Your token isn\'t in database. Type the command /token')
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
@@ -58,12 +95,8 @@ async def get_me(message: types.Message):
     This handler will be called when user sends `/start` or `/help` command
     """
     user_id = message.from_user.id
-    db = Client('password')
-    data = db.get({'telegram_id': user_id})
-    hasher = Hasher()
-    if data:
-        encrypted_token = data.get('token')
-        decrypted_token = hasher.decrypt_message(encrypted_token)
+    decrypted_token = await decrypt_token(user_id)
+    if decrypted_token:
         info = Api(decrypted_token)
         return await message.answer(info.get_user_info(), parse_mode='Markdown')
     else:
@@ -76,22 +109,24 @@ async def get_repos(message: types.Message):
     This handler will be called when user sends `/start` or `/help` command
     """
     user_id = message.from_user.id
-    db = Client('password')
-    data = db.get({'telegram_id': user_id})
-    hasher = Hasher()
-    if data:
-        encrypted_token = data.get('token')
-        decrypted_token = hasher.decrypt_message(encrypted_token)
+    decrypted_token = await decrypt_token(user_id)
+    if decrypted_token:
         info = Api(decrypted_token)
         repos = info.get_repos()
         text = ''
         index = 1
+        inline_keyboard = types.InlineKeyboardMarkup(row_width=5)
+        buttons = []
         for repo in repos:
             if not repo.archived:
                 text += f'{index}. {repo.name}. [Link]({repo.html_url}). ' \
                           f'Total issues and prs: {repo.get_issues().totalCount}\n'
+                button = types.InlineKeyboardButton(index, callback_data=repo.name)
+                buttons.append(button)
                 index += 1
-        return await message.answer(text, parse_mode='Markdown')
+        inline_keyboard.add(*buttons)
+        text += '\nClick the number of repository to get *details*'
+        return await message.answer(text, parse_mode='Markdown', reply_markup=inline_keyboard)
     else:
         return await message.answer('Your token isn\'t in database. Type the command /token')
 
@@ -102,12 +137,8 @@ async def get_issues(message: types.Message):
     This handler will be called when user sends `/start` or `/help` command
     """
     user_id = message.from_user.id
-    db = Client('zxcVBN0911<>')
-    data = db.get({'telegram_id': user_id})
-    hasher = Hasher()
-    if data:
-        encrypted_token = data.get('token')
-        decrypted_token = hasher.decrypt_message(encrypted_token)
+    decrypted_token = await decrypt_token(user_id)
+    if decrypted_token:
         info = Api(decrypted_token)
         return await message.answer(info.get_issues(), parse_mode='Markdown')
     else:
@@ -117,26 +148,26 @@ async def get_issues(message: types.Message):
 @dp.message_handler()
 async def echo(message: types.Message):
     user_id = message.from_user.id
-    db = Client('password')
-    hasher = Hasher()
-    data = db.get({'telegram_id': user_id})
-    encrypted_token = data.get('token')
-    decrypted_token = hasher.decrypt_message(encrypted_token)
-    info = Api(decrypted_token)
-    data = info.get_repo(message.text)
-    if data:
-        inline_keyboard = types.InlineKeyboardMarkup()
-        for issue in data['issues']:
-            if not issue.pull_request:
-                button = types.InlineKeyboardButton(f'Issue #{issue.number} - {issue.title}', url=issue.html_url)
-            else:
-                button = types.InlineKeyboardButton(f'Pull request #{issue.number} - {issue.title}', url=issue.html_url)
-            inline_keyboard.add(button)
-        final_text = f"Name: *{data['name']}*\nLink: [click here]({data['url']})\n" \
-                     f"Stars: *{data['stars']}*\nTotal issues and prs: *{data['count_of_issues']}*"
-        await message.answer(final_text, reply_markup=inline_keyboard, parse_mode='Markdown')
+    decrypted_token = await decrypt_token(user_id)
+    if decrypted_token:
+        info = Api(decrypted_token)
+        data = info.get_repo(message.text)
+        if data:
+            inline_keyboard = types.InlineKeyboardMarkup()
+            for issue in data['issues']:
+                if not issue.pull_request:
+                    button = types.InlineKeyboardButton(f'Issue #{issue.number} - {issue.title}', url=issue.html_url)
+                else:
+                    button = types.InlineKeyboardButton(f'Pull request #{issue.number} - {issue.title}', url=issue.html_url)
+                inline_keyboard.add(button)
+            final_text = f"Name: *{data['name']}*\nLink: [click here]({data['url']})\n" \
+                         f"Stars: *{data['stars']}*\nTotal issues and prs: *{data['count_of_issues']}*"
+            await message.answer(final_text, reply_markup=inline_keyboard, parse_mode='Markdown')
+        else:
+            await message.answer('Couldn\'t find your repository')
     else:
-        await message.answer('Couldn\'t find your repository')
+        return await message.answer('Your token isn\'t in database. Type the command /token')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
