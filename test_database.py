55f5db61ba41b91a85bd3f03d2648f4803b310ea
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-24 17:17:21 +0300

    Update test_database.py

--- test_database.py
+++ test_database.py
@@ -3,15 +3,16 @@ from unittest import TestCase
 
 from database import Client
 
-# TODO: Write integration tests
+
 class Parent:
     client = Client(os.getenv('MONGO_PASSWORD', 'password'), 'githubhelper', 'tokens')
-    data = {'test_token': 'test_token', 'test_id': 'test_id'}
+    full_data = {'test_token': 'test_token', 'test_id': 'test_id'}
+    data_with_id = {'test_id': 'test_id'}
 
 
 class TestClient1(TestCase, Parent):
     def test_insert(self):
-        result = self.client.insert(self.data)
+        result = self.client.insert(self.full_data)
         self.assertTrue(result.acknowledged)
         self.assertIsNotNone(result.inserted_id)
 
@@ -21,20 +22,20 @@ class TestClient2(TestCase, Parent):
         result = self.client.get({'test_token': 'test_token'})
         self.assertIsNotNone(result)
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['test_id'], self.data['test_id'])
+        self.assertEqual(result['test_id'], self.full_data['test_id'])
 
     def test_update(self):
         result = self.client.update({'test_token': 'test_token'}, {'test_token': 'test_token1'})
         self.assertTrue(result.acknowledged)
         self.assertIsNotNone(result.modified_count)
-        updated_data = self.client.get({'test_id': 'test_id'})
+        updated_data = self.client.get(self.data_with_id)
         self.assertEqual(updated_data['test_token'], 'test_token1')
 
 
 class TestClient3(TestCase, Parent):
     def test_delete(self):
-        result = self.client.delete({'test_id': 'test_id'})
+        result = self.client.delete(self.data_with_id)
         self.assertIsNotNone(result)
         self.assertTrue(result.acknowledged)
-        data = self.client.get({'test_id': 'test_id'})
+        data = self.client.get(self.data_with_id)
         self.assertIsNone(data)
