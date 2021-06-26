f8c4ef3d1d4162ce825af3401c4a7334420aa5dc
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-26 20:31:28 +0300

    #1 Update api.py
    Add docstrings

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-# TODO: Add docstrings
 # TODO: Add tests
 from github import Github
 from github.GithubException import UnknownObjectException
@@ -9,7 +8,14 @@ from github.Repository import Repository
 
 
 class Api:
-    def __init__(self, token):
+    """
+    Class for manipulating with GitHub API
+    """
+    def __init__(self, token: str):
+        """
+        Create the authenticated user as object
+        :param token: token for GitHub API
+        """
         self.g = Github(token)
         self.user = self.g.get_user()
 
@@ -21,10 +27,19 @@ class Api:
         return f'You have been authenticated with login *{self.user.login}* as *{self.user.name}*'
 
     def get_repos(self) -> PaginatedList:
+        """
+        Method for getting all user repositories
+        :return: list of repositories
+        """
         repos = self.user.get_repos()
         return repos
 
     def get_repo(self, repo_name: str) -> Repository:
+        """
+        Method for getting one repository
+        :param repo_name: short name of the repository
+        :return:
+        """
         try:
             repo = self.user.get_repo(repo_name)
             return repo
@@ -32,6 +47,11 @@ class Api:
             return None
 
     def get_issues_or_prs(self, option: bool) -> list:
+        """
+        Method for getting oll issues or pull requests
+        :param option: issues - True, pull requests - False
+        :return: list of items
+        """
         # TODO: get user authored issues and prs
         items = self.user.get_issues()
         result = []
@@ -40,13 +60,23 @@ class Api:
                 result.append(item)
         return result
 
-    def close_issues_or_prs(self, url) -> None:
+    def close_issues_or_prs(self, url: str) -> None:
+        """
+        Method for closing issues or pull requests
+        :param url: api url for closing
+        :return: nothing to return
+        """
         items = self.user.get_issues()
         for item in items:
             if item.url == url:
                 item.edit(state='closed')
 
-    def merge_prs(self, url) -> None:
+    def merge_prs(self, url: str) -> None:
+        """
+        Method for merging pull requests
+        :param url: api url for merging
+        :return: nothing to return
+        """
         items = self.user.get_issues()
         for item in items:
             if item.url == url:
@@ -56,6 +86,11 @@ class Api:
                 pr.merge()
 
     def create_issue(self, data: dict) -> Issue:
+        """
+        Method for creating issue
+        :param data: meta-data for issue
+        :return: Issue object
+        """
         repo = self.get_repo(data['RepoName'])
         try:
             issue = repo.create_issue(
@@ -68,6 +103,11 @@ class Api:
             return None
 
     def create_pr(self, data: dict) -> PullRequest:
+        """
+        Method for creating pull request
+        :param data: meta-data for pull request
+        :return: PullRequest object
+        """
         repo = self.get_repo(data['RepoName'])
         try:
             pr = repo.create_pull(
