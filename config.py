215ff355abb8d3fd6bb79df3a4fb30f8682a5931
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-29 16:57:33 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_TOKEN = os.getenv('TELEGRAM_TOKEN', 'telegram_token')
+DB_PASSWORD = os.getenv('MONGO_PASSWORD', 'password_to_database')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', 'github_token')
