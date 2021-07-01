bb1aa962b83ac82af94e360c6f345d3692b9da32
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-01 13:45:12 +0300

    Update test_database.py
    Update tests

--- test_database.py
+++ test_database.py
@@ -1,3 +1,5 @@
+import sys
+
 from unittest import TestCase
 
 from database import Client
@@ -6,7 +8,7 @@ from config import DB_PASSWORD
 
 class Parent:
     client = Client(DB_PASSWORD, 'githubhelper', 'tokens')
-    full_data = {'test_token': 'test_token', 'test_id': 'test_id'}
+    full_data = {'test_token': 'test_token' + sys.version, 'test_id': 'test_id'}
     data_with_id = {'test_id': 'test_id'}
 
 
@@ -19,17 +21,17 @@ class TestClient1(TestCase, Parent):
 
 class TestClient2(TestCase, Parent):
     def test_get(self):
-        result = self.client.get({'test_token': 'test_token'})
+        result = self.client.get({'test_token': 'test_token' + sys.version})
         self.assertIsNotNone(result)
         self.assertIsInstance(result, dict)
         self.assertEqual(result['test_id'], self.full_data['test_id'])
 
     def test_update(self):
-        result = self.client.update({'test_token': 'test_token'}, {'test_token': 'test_token1'})
+        result = self.client.update({'test_token': 'test_token' + sys.version}, {'test_token': 'test_token1' + sys.version})
         self.assertTrue(result.acknowledged)
         self.assertIsNotNone(result.modified_count)
         updated_data = self.client.get(self.data_with_id)
-        self.assertEqual(updated_data['test_token'], 'test_token1')
+        self.assertEqual(updated_data['test_token'], 'test_token1' + sys.version)
 
 
 class TestClient3(TestCase, Parent):
