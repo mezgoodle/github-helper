bf6892f751bf3d527ea20e3b94bba7711b3a08f7
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-03 15:22:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -504,7 +504,7 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = int(os.getenv('PORT'), 5000)
+WEBAPP_PORT = int(os.getenv('PORT', 5000))
 
 
 async def on_startup(dp):
