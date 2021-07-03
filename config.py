e769aba150676743b3d1766e49f0ce38fdea1c07
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-03 14:48:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 API_TOKEN = os.getenv('TELEGRAM_TOKEN', 'telegram_token')
 DB_PASSWORD = os.getenv('MONGO_PASSWORD', 'password_to_database')
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', 'github_token')
-HASH_KEY = os.getenv('HAS_KEY', b'Kt7ioOW4eugqDkfqcYiCz2mOuyiWRg_MTzckxEVp978=')
+HASH_KEY = os.getenv('HASH_KEY', 'Kt7ioOW4eugqDkfqcYiCz2mOuyiWRg_MTzckxEVp978=')
