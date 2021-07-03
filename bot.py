a2f962ab98b810b3c04390577efbb71962b21b81
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-03 15:31:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -496,17 +496,18 @@ async def echo(message: types.Message) -> types.Message:
         return await message.answer('Your token isn\'t in database. Type the command /token')
 
 
-# webhook settings
+# Webhook settings
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-# webserver settings
+# Webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.getenv('PORT', 5000))
 
 
+# Functions for webhooks
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
 
